b2a1aa6aa6f1c51736727d67a568932f97f82a88
belalgamal46 <32738452+belalgamal46@users.noreply.github.com>
2020-08-05 08:34:34 +0200

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+let dice1Path = "images/dice1.png";
+let dice2Path = "images/dice2.png";
+let dice3Path = "images/dice3.png";
+let dice4Path = "images/dice4.png";
+let dice5Path = "images/dice5.png";
+let dice6Path = "images/dice6.png";
+
+
+let arrayOfDice = [
+    dice1Path,
+    dice2Path,
+    dice3Path,
+    dice4Path,
+    dice5Path,
+    dice6Path
+];
+
+
+function changeImg(id) {
+    let random = Math.floor((Math.random() * arrayOfDice.length));
+    let path = arrayOfDice[random];
+    window.onload = document.querySelector(id).setAttribute("src", path);
+    return random;
+}
+
+let dice1 = changeImg("#dice1");
+let dice2 = changeImg("#dice2");
+
+function winningPlayer() {
+    if (dice1 > dice2) {
+        document.querySelector("#head").textContent = "Player 1 Wins !!";
+    } else if (dice1 == dice2) {
+        document.querySelector("#head").textContent = "Draw";
+    } else {
+        document.querySelector("#head").textContent = "Player 2 Wins !!";
+    }
+}
+
+window.onload = winningPlayer();
